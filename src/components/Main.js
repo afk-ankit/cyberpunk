daf5d9fda612f0d0b90d721c00c8507dd10005d1
Ankit <ankitsharmagh093@gmail.com>
2022-10-07 12:33:38 +0530

    deployed on vercel

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,50 +1,50 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main.css'
 import eth from './Assests/weth.png'
 import insta from './Assests/owner/instagram.png'
 import twweet from './Assests/owner/twitter.png'
 import more from './Assests/owner/more.png'
 
-function Main({ Punks ,selectedPunk}) {
+function Main({ Punks, selectedPunk }) {
     const [activePunk, setActivePunk] = useState(Punks[0])
-        
-       useEffect(()=>{
-           setActivePunk(Punks[selectedPunk])
-       },[Punks,selectedPunk])
 
-        return (<div>
-            <div className="mainContainer">
-                <div className="mainimgContainer">
-                    <img src={activePunk.img} alt="" />
+    useEffect(() => {
+        setActivePunk(Punks[selectedPunk])
+    }, [Punks, selectedPunk])
+
+    return (<div>
+        <div className="mainContainer">
+            <div className="mainimgContainer">
+                <img src={activePunk.img} alt="" />
+            </div>
+            <div className='titleContainer'>
+                <div className="mainTitle">
+                    #{activePunk.name}
                 </div>
-                <div className='titleContainer'>
-                    <div className="mainTitle">
-                        #{activePunk.name}
-                    </div>
-                    <div className="mainId">
-                        .0{activePunk.id}
-                    </div>
-                    {/* <div className="mainPrice">
+                <div className="mainId">
+                    .0{activePunk.id}
+                </div>
+                {/* <div className="mainPrice">
                         .{activePunk.price}<img src={eth} alt="" />
                     </div> */}
+            </div>
+            <div className="mainSocials">
+                <div className="insta">
+                    <img src={insta} alt="" />
+                    @_ankixt_
                 </div>
-                <div className="mainSocials">
-                    <div className="insta">
-                        <img src={insta} alt="" />
-                        @_ankixt_
-                    </div>
-                    <div className="insta">
-                        <img src={twweet} alt="" />
-                        @ankitSharma4645
-                    </div>
-                    <div className="insta">
-                        <img src={more} alt="" />
-                        more
-                    </div>
+                <div className="insta">
+                    <img src={twweet} alt="" />
+                    @ankitSharma4645
+                </div>
+                <div className="insta">
+                    <img src={more} alt="" />
+                    more
                 </div>
             </div>
         </div>
-        )
-    }
+    </div>
+    )
+}
 
-    export default Main;
+export default Main;
--- src/components/Punklist.js
+++ src/components/Punklist.js
@@ -3,16 +3,21 @@ import React from "react";
 import CollectionCard from "./CollectionCard";
 import "./Punklist.css";
 
-const Punklist = ({ Punks ,  setSelectedPunk }) => {
-    let getData=(a)=>{
-        setSelectedPunk(a);
-        // console.log(a)
-    }
+const Punklist = ({ Punks, setSelectedPunk }) => {
+  let getData = (a) => {
+    setSelectedPunk(a);
+    // console.log(a)
+  };
   return (
     <div className="punkListContainer">
       <div className="punkList">
         {Punks.map((punk) => (
-          <CollectionCard  className="PU" key={punk.id} data={punk} getData={getData}/>
+          <CollectionCard
+            className="PU"
+            key={punk.id}
+            data={punk}
+            getData={getData}
+          />
         ))}
       </div>
     </div>
